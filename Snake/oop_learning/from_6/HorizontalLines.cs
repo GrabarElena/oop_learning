3448ff5f1575fe8a2325721af31c742cb3fa90f5
Grabar Elena <lenka_ru@hotmail.com>
2018-01-21 23:56:32 +0300

    comment

---
+++ Snake/oop_learning/from_6/HorizontalLines.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_learning
+{
+    class HorizontalLines
+    {
+        List<Point> pList;
+
+        public HorizontalLines()
+        {
+            pList = new List<Point>();
+            Point p1 = new Point(4, 4, '*');
+            Point p2 = new Point(4, 5, '*');
+            Point p3 = new Point(4, 6, '*');
+        }
+    }
+}
