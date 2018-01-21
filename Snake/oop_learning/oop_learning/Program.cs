a9f7846b59c5006fab240bb40adc1469f191a830
Grabar Elena <lenka_ru@hotmail.com>
2018-01-21 21:28:35 +0300

    результаты 2-го занятия

--- Snake/oop_learning/oop_learning/Program.cs
+++ Snake/oop_learning/oop_learning/Program.cs
@@ -8,9 +8,33 @@ namespace oop_learning
 {
     class Program
     {
+        /// <summary>
+        /// Вывести в координатах x, y значение переменной symb
+        /// </summary>
+        /// <param name="x">координата X</param>
+        /// <param name="y">координата Y</param>
+        /// <param name="symb">чего выводим</param>
+        static void draw(int x, int y, char symb)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(symb);
+        }
+
         static void Main(string[] args)
         {
+            int x1 = 1;
+            int y1 = 2;
+            char symb1 = '*';
 
+            draw(x1, y1, symb1);
+
+            int x2 = 3;
+            int y2 = 5;
+            char symb2 = '#';
+
+            draw(x2, y2, symb2);
+
+            Console.ReadLine();
         }
     }
 }
