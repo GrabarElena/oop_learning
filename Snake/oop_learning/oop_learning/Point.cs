a03b2f5f85ab0a3a5235fe734dbfa6092138b223
Grabar Elena <lenka_ru@hotmail.com>
2018-01-21 21:56:48 +0300

    На третьем уровке сделали класс Point,  куда свалили всё переиспользуемое.

---
+++ Snake/oop_learning/oop_learning/Point.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_learning
+{
+    class Point
+    {
+        //Для того, тчобы можно было использовать эти переменные извне этого класса - тип public
+        public int x;
+        public int y;
+        public char symb;
+
+        //когда это быо методом - описывалось следующей конструкцией: static void draw(int x, int y, char symb)
+        public void draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(symb);
+        }
+    }
+}
